92c73f9ba7bdc1c24bf51f572ec0e13340deb2a2
Venkata Kishore <vedamk@gmail.com>
2023-09-07 20:30:47 +0300

    update as on 07092023

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ SECRET_KEY = "FLASKVEDSECKEY"
 # JWT configuration
 JWT_SECRET_KEY = "JWTVEDSECKEY"
 #JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=3600)
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=60)
-JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=5)
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
 
 # config.py
 APPLICATION_CREDENTIALS = [
-    {"userid":"0","username": "kishore", "password": "$2b$12$OjcDyH3BflTBHiiiQ1pVtOwWnxFju6j7EUBG8GWZKeKICVghjeI96"},
-    {"userid":"1","username": "admin", "password": "$2b$12$gnuMPXgfBzU4HqpUwFEVBu71oTAPPZfgnW0GCi50R0rsxeleIN042"},
-    # Add more username-password pairs as needed the userids must be below 100
+    {"userid":"0","username": "kishore", "name":"SUPER USER0" , "password": "$2b$12$OjcDyH3BflTBHiiiQ1pVtOwWnxFju6j7EUBG8GWZKeKICVghjeI96"},
+    {"userid":"1","username": "admin", "name":"SUPER USER1" ,"password": "$2b$12$gnuMPXgfBzU4HqpUwFEVBu71oTAPPZfgnW0GCi50R0rsxeleIN042"},
+   #{"userid":"100-MAX value","username": "admin", "name":"SUPER USER1" ,"password": "$2b$12$gnuMPXgfBzU4HqpUwFEVBu71oTAPPZfgnW0GCi50R0rsxeleIN042"}   
+    # Add more username-password pairs as needed the userids must be below 100. This count should match with the SUPER_USERS_COUNT in application
 ]
--- modules/security/login_user.py
+++ modules/security/login_user.py
@@ -10,7 +10,6 @@ from config import JWT_ACCESS_TOKEN_EXPIRES, APPLICATION_CREDENTIALS, JWT_REFRES
 
 login_data_api = Blueprint('login_data_api', __name__)
 
-
 @login_data_api.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
@@ -25,28 +24,28 @@ def login():
             password.encode('utf-8'), user_info["password"].encode('utf-8')
         ):
             # Passwords match, generate and return a session token or JWT
-            print("The User is in the Password Pair list in the config file : credential are matched", int(
-                user_info["userid"]))
+            print("The User is in the Password Pair list in the config file : credential are matched", int(user_info["userid"]))
             access_token = create_access_token(
                 identity=username, expires_delta=current_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
-            )
+                )
 
             refresh_token = create_refresh_token(
                 identity=username,
-                expires_delta=JWT_REFRESH_TOKEN_EXPIRES
+                    expires_delta=JWT_REFRESH_TOKEN_EXPIRES
             )
 
             # refresh_token = create_refresh_token(identity=username)
             print("Application user details --> ", username,
-                  int(user_info["userid"]), access_token)
+              int(user_info["userid"]), access_token)
             print(refresh_token)
             return jsonify({
                 "access_token": access_token,
                 "refresh_token": refresh_token,
                 "username": username,
-                "userid": int(user_info["userid"])
-            })
-
+                "userid": int(user_info["userid"]),
+                "empid": 0,
+                "name" : user_info["name"]
+                })
     # If the username-password pair is not found in the array, check the database
     print("The User is not in the Password Pair list in the config file : credential will be checked in the db")
     mydb = get_database_connection()
@@ -79,13 +78,34 @@ def login():
             print("Stored user details --> ",
                   access_token, stored_username, userid)
             print(refresh_token)
-            return jsonify({
-                "access_token": access_token,
-                "refresh_token": refresh_token,
-                "username": stored_username,
-                "userid": userid
-            })
 
+            query1 = "SELECT name FROM com.employee WHERE empid = %s"
+            values = (int(empid),)
+            mycursor1 = mydb.cursor()
+            mycursor1.execute(query1, values)
+            result1 = mycursor1.fetchone()
+            print("Input Employee id is -->",empid)
+            if result1:
+                fetched_name = result1[0]
+                mycursor1.close()
+                return jsonify({
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "username": stored_username,
+                    "userid": userid,
+                    "empid" : empid,
+                    "name" : fetched_name
+                })
+            else:
+                # Handle the case when result1 is not truthy
+                return jsonify({
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "username": stored_username,
+                    "userid": userid,
+                    "empid": empid,
+                    "name": "NO NAME IN DB"  # You can specify a default value here, such as None
+                })
     # Close the cursor and connection
     mycursor.close()
     mydb.close()
