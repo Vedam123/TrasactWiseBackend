934b4eefd8ba0c255fe9d4602c08b4ae1fdae796
Vedam123 <vedamk@gmail.com>
2025-02-09 14:06:56 +0200

    uninstall processes

--- Documents/SetupGuides/ConfigFiles/config/instance/991_PROD_WebClient_Stop.bat
+++ Documents/SetupGuides/ConfigFiles/config/instance/991_PROD_WebClient_Stop.bat
@@ -47,6 +47,10 @@ if errorlevel 1 (
 echo Removing PM2 startup setup...
 npx pm2 unstartup
 
+:: Call the PowerShell script in the same directory and pass the keyword as a parameter
+powershell.exe -ExecutionPolicy Bypass -File "%current_dir%991_PROD_KISS_STUCK_PROCESSES.ps1" -keyword "%company%"
+
+
 echo All PM2 processes stopped and startup settings removed.
 echo Press any key to exit...
 pause >nul
